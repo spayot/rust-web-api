275af4727b59eedad42a965afc43c3cd2673606d
Sylvain Payot <sylvain.payot@gmail.com>
2023-12-02 14:31:48 -0800

    moving macros to separate module

--- src/handlers.rs
+++ src/handlers.rs
@@ -4,42 +4,11 @@ use crate::models::Post;
 use iron::headers::ContentType;
 use iron::{status, AfterMiddleware, Handler, IronResult, Request, Response};
 use router::Router;
-// use std::error::Error;
 use std::io::Read;
 use std::sync::{Arc, Mutex};
 use uuid::Uuid;
 
-macro_rules! try_handler {
-    ($e:expr) => {
-        match $e {
-            Ok(x) => x,
-            Err(e) => return Ok(Response::with((status::InternalServerError, e.to_string()))),
-        }
-    };
-    ($e:expr, $error:expr) => {
-        match $e {
-            Ok(x) => x,
-            Err(e) => return Ok(Response::with(($error, e.to_string()))),
-        }
-    };
-}
-
-macro_rules! lock {
-    ($e:expr) => {
-        $e.lock().unwrap()
-    };
-}
-macro_rules! get_http_param {
-    ($r:expr, $e:expr) => {
-        match $r.extensions.get::<Router>() {
-            Some(router) => match router.find($e) {
-                Some(v) => v,
-                None => return Ok(Response::with((status::BadRequest))),
-            },
-            None => return Ok(Response::with((status::InternalServerError))),
-        }
-    };
-}
+use crate::{get_http_param, lock, try_handler};
 
 pub struct Handlers {
     pub post_feed: PostFeedHandler,
